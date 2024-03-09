a27efd1925216506dc1305cf375e64f8786c147f
AnaKuenneth <anakuenneth@hotmail.com>
2024-03-09 05:00:34 -0300

    Ejercicios con String

--- ejercicioBuclesString13.js
+++ ejercicioBuclesString13.js
@@ -1,2 +1,6 @@
 //Ejercicios con String
 /*13- Realiza un script que pida un texto y lo muestre en mayúsculas. */
+
+let texto = prompt('Ingresa un texto. ¡Te lo mostraré en Mayúculas!')
+console.log(texto.toUpperCase())
+alert(texto.toUpperCase())
